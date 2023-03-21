31f31000aabe57ae8a29b223b4650edb4b96d28a
Riccardo Serino <113998283+FenicEzz@users.noreply.github.com>
2023-03-21 11:38:03 +0100

    Add files via upload

---
+++ Week 1/Giorno 3/Ex3/d3-Esercizio3.cs
@@ -0,0 +1,29 @@
+﻿namespace Giorno_3___ex3
+{
+    internal class Program
+    {
+        static void OperazioniArray(int[] arr)
+        {
+            double sum = 0;
+
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+
+            double average = sum / arr.Length;
+
+            Console.WriteLine(average);
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+            int[] numbers2 = { 12, 23, 34, 45, 56, 68 };
+
+            OperazioniArray(numbers);
+            OperazioniArray(numbers2);
+        }
+    }
+}
