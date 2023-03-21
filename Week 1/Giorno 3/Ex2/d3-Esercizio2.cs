cca18c8ace9954eeb176fcaeae68b17d043753e1
Riccardo Serino <113998283+FenicEzz@users.noreply.github.com>
2023-03-21 11:36:56 +0100

    Add files via upload

---
+++ Week 1/Giorno 3/Ex2/d3-Esercizio2.cs
@@ -0,0 +1,36 @@
+﻿namespace Giorno_3___ex2
+{
+    internal class Esercizio2
+    {
+
+        static void RicercaNome(string[] arr)
+        {
+            Console.WriteLine("Inserire nome da cercare: ");
+            string nomeRichiesto = Console.ReadLine();
+            bool x = false;
+
+            foreach (string s in arr)
+            {
+                if (s == nomeRichiesto)
+                {
+                    x = true;
+                }
+            }
+
+            if (x) 
+            {
+                Console.WriteLine($"{nomeRichiesto} esiste!");
+            }
+            else
+            {
+                Console.WriteLine($"{nomeRichiesto} è mancante!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] nomi = new string[] { "Riccardo", "Marco", "Alice", "Giulia" };   
+            
+            RicercaNome(nomi);
+        }
+    }
+}
